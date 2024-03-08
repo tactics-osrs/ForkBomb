e36e9054beea7b8dd91e4f080b3e9fb7fb293f89
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2024-03-07 22:13:31 -0500

    Create ForkBomb.bat

---
+++ ForkBomb.bat
@@ -0,0 +1,23 @@
+REM Do not run this without fully understanding what a fork bomb is.
+REM It keeps splitting itself until there is no option but to restart the system.
+REM Should you accidentally run this, you can close it via the task manager if you are fast enough.
+@echo off
+
+echo.
+echo You are about to start a fork bomb. Are you sure?
+echo.
+
+:choice
+set "input="
+set /p input="Enter your choice (yes/no): "
+
+if /I "%input%"=="yes" (
+	echo Starting fork bomb...
+	%0|%0
+) else if /I "%input%"=="no" (
+    echo Exiting without starting fork bomb...
+    exit /b
+) else (
+	echo Invalid input. Please enter either "yes" or "no".
+	goto choice
+)
